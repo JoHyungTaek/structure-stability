0e3feaee8aa3881d4386b6920755d5c653a056f3
JoHyungTaek <JoHyungTaek@gmail.com>
2026-03-13 13:36:55 +0900

    성능 개선을 위한 전체적인수정

--- configs/config.py
+++ configs/config.py
@@ -1,18 +1,16 @@
 CFG = {
-    "IMG_SIZE": 380,
+    "IMG_SIZE": 384,
     "EPOCHS": 12,
     "LEARNING_RATE": 5e-5,
     "BATCH_SIZE": 8,
-    "ACCUM_STEPS": 2,
     "SEED": 42,
     "NUM_WORKERS": 2,
-    "MODEL_NAME": "efficientnet_b4",
+    "MODEL_NAME": "efficientnet_b3",
     "DROPOUT": 0.4,
     "WEIGHT_DECAY": 2e-4,
     "AMP": True,
     "PATIENCE": 3,
     "TTA": True,
-    "LABEL_SMOOTHING": 0.02,
 }
 
 BASE_PATH = "/content/drive/MyDrive/데이콘/open"
